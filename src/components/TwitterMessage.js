174ac36a9ec251076a2bfb6b033cfac510967b00
Noam Sauer-Utley <noamsauerutley@gmail.com>
2019-11-20 17:16:39 -0500

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,17 +4,25 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      body: ""
+    };
+  }
+  handleCharacterCount = () => {
+    let remainingChars = this.props.maxChars - this.state.body.length
+    return remainingChars
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={event => this.setState({body: event.target.value})} value={this.state.body}/>
+        {this.handleCharacterCount()}
       </div>
     );
   }
 }
 
 export default TwitterMessage;
+ 
